05d74dd3b7fc719f406a2598155c96f994964267
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-08-23 20:02:56 +0200

    Add files via upload

---
+++ src/4.ts
@@ -0,0 +1,66 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} увійшов в хату.`);
+    } else {
+      console.log("Двері зачинено.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Двері відчинено");
+    } else {
+      console.log("Ключ не підходить.");
+    }
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
